97aefc04ba3d842d1fc8ba9d6a350a7438b185d4
Matt Topol <zotthewizard@gmail.com>
2026-03-30 13:02:10 -0400

    fix(rust): Quote OPENSSL_DIR value in .env.build for Windows

--- rust/ci/scripts/pre-build.sh
+++ rust/ci/scripts/pre-build.sh
@@ -1,1 +1,7 @@
-if [[ "$2" == "windows" ]]; then echo "OPENSSL_DIR=C:\Program Files\OpenSSL-Win64" > .env.build; fi
+#!/usr/bin/env bash
+
+set -ex
+
+if [[ "$2" == "windows" ]]; then
+    echo "OPENSSL_DIR='C:\Program Files\OpenSSL-Win64'" > .env.build
+fi
